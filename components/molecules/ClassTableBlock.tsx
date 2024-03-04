32a2cb3853a5b172af40a99aa0d628f990e93bd4
zHannur0 <zhannurkhan04@gmail.com>
2024-03-04 14:08:25 +0500

    fixed bugs

--- components/molecules/ClassTableBlock.tsx
+++ components/molecules/ClassTableBlock.tsx
@@ -99,8 +99,8 @@ const ClassTableBlock: FC<IProps> = ({
                   language: values.language,
                   classroom: values.cabinet,
                   class_teacher: values.classRuk,
-                  osnova_plan: Number(values.calls1),
-                  dopurok_plan: Number(values.calls2),
+                  osnova_plan: values.calls1 || null,
+                  dopurok_plan: values.calls2 || null,
                     osnova_smena: smena1,
                     dopurak_smena: smena2,
 
