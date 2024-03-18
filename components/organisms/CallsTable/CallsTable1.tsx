42876958ee6646933b6678e83af12c0ce7687fe9
zHannur0 <zhannurkhan04@gmail.com>
2024-03-18 13:30:00 +0500

    new CallsTable

--- components/organisms/CallsTable/CallsTable1.tsx
+++ components/organisms/CallsTable/CallsTable1.tsx
@@ -2,13 +2,14 @@ import { useAppDispatch } from "@/hooks/useAppDispatch";
 import { DeleteIcons, PenIcons } from "../../atoms/Icons";
 import { Table, Td, Th, Thead, Tr } from "../../atoms/UI/Tables/Table";
 import { useTypedSelector } from "@/hooks/useTypedSelector";
-import { FC, useEffect } from "react";
+import {FC, useEffect, useState} from "react";
 import { getOSThunk } from "@/store/thunks/pride.thunk";
 import { instance } from "@/api/axios.instance";
 import {
   getTokenInLocalStorage,
   removeSecondOfTime,
 } from "@/utils/assets.utils";
+import {ICalls} from "@/types/assets.type";
 
 interface IProps {
   handleClickGetIdOS?: (id?: number) => void;
@@ -17,12 +18,31 @@ interface IProps {
 const CallsTable: FC<IProps> = ({ handleClickGetIdOS }) => {
   const dispatch = useAppDispatch();
   const os = useTypedSelector((state) => state.pride.os);
-
+  const [calls, setCalls] = useState<ICalls[][]>([]);
   useEffect(() => {
     if (os) {
       dispatch(getOSThunk());
+
     }
   }, [dispatch]);
+
+  useEffect(() => {
+    if(os) {
+      const groupByPlan = () => {
+        const groups: { [key: number]: ICalls[] } = {};
+        for (const item of os) {
+          if (item.plan !== undefined) {
+            if (!groups[item.plan]) {
+              groups[item.plan] = [];
+            }
+            groups[item.plan].push(item);
+          }
+        }
+        return Object.values(groups);
+      };
+      setCalls(groupByPlan);
+    }
+  }, [os]);
 
   const handleDeleteItems = async (id?: number) => {
     await instance
@@ -44,43 +64,67 @@ const CallsTable: FC<IProps> = ({ handleClickGetIdOS }) => {
     <div className="main_table">
       <div className="main_table-title">Основной урок </div>
 
-      <div className="main_table-block">
-        <Table>
-          <Thead>
-            <tr>
-              <Th>План звонков</Th>
-              <Th>Номер урока</Th>
-              <Th>Смена</Th>
-              <Th>Начало урока</Th>
-              <Th>Конец урока</Th>
-              <Th>Действие</Th>
-            </tr>
-          </Thead>
+      <div className="main_table-block" style={{display: "flex", flexWrap: "wrap", gap: "4%"}}>
+        {
+          calls &&
+            calls.map((call, index) => (
+                    <div style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "15px",
+                      width: "48%",
+                      padding: "21px",
+                      background: "#F4F4F4",
+                      borderRadius: "11.429px",
+                      marginBottom: "20px",
+                    }} key={index}>
+                      <div style={{color: "#D12E34", fontWeight: "bold"}}>
+                        План звонков: <span style={{color: "black"}}>{call[0].plan}</span>
+                      </div>
+                      <div style={{color: "#D12E34", fontWeight: "bold"}}>
+                        Смена: <span style={{color: "black"}}>{call[0].smena}</span>
+                      </div>
+                      <div>
 
-          {os &&
-            os.map((item, index) => (
-              <Tr key={item.plan}>
-                <Td>{item.plan}</Td>
-                <Td>{item.number}</Td>
-                <Td>{item.smena}</Td>
-                <Td>{removeSecondOfTime(item.start_time)}</Td>
-                <Td>{removeSecondOfTime(item.end_time)}</Td>
-                <Td>
-                  <div
-                    onClick={() =>
-                      handleClickGetIdOS && handleClickGetIdOS(item.id)
-                    }
-                  >
-                    <PenIcons />
-                  </div>
+                      </div>
+                      <Table>
+                        <Thead>
+                          <tr>
+                            <Th>№</Th>
+                            <Th>Начало урока</Th>
+                            <Th>Конец урока</Th>
+                            <Th>Действия</Th>
+                          </tr>
+                        </Thead>
 
-                  <div onClick={() => handleDeleteItems(item.id)}>
-                    <DeleteIcons />
-                  </div>
-                </Td>
-              </Tr>
-            ))}
-        </Table>
+                        {call &&
+                            call.map((item, index) => (
+                                <Tr key={item.plan}>
+                                  <Td>{item.number}</Td>
+                                  <Td>{removeSecondOfTime(item.start_time)}</Td>
+                                  <Td>{removeSecondOfTime(item.end_time)}</Td>
+                                  <Td>
+                                    <div
+                                        onClick={() =>
+                                            handleClickGetIdOS && handleClickGetIdOS(item.id)
+                                        }
+                                    >
+                                      <PenIcons/>
+                                    </div>
+
+                                    <div onClick={() => handleDeleteItems(item.id)}>
+                                      <DeleteIcons/>
+                                    </div>
+                                  </Td>
+                                </Tr>
+                            ))}
+                      </Table>
+                    </div>
+
+                )
+            )
+        }
+
       </div>
     </div>
   );
--- components/organisms/CallsTable/CallsTable2.tsx
+++ components/organisms/CallsTable/CallsTable2.tsx
@@ -2,13 +2,14 @@ import { useAppDispatch } from "@/hooks/useAppDispatch";
 import { DeleteIcons, PenIcons } from "../../atoms/Icons";
 import { Table, Td, Th, Thead, Tr } from "../../atoms/UI/Tables/Table";
 import { useTypedSelector } from "@/hooks/useTypedSelector";
-import { FC, useEffect } from "react";
+import {FC, useEffect, useState} from "react";
 import { getDopThunk } from "@/store/thunks/pride.thunk";
 import { instance } from "@/api/axios.instance";
 import {
   getTokenInLocalStorage,
   removeSecondOfTime,
 } from "@/utils/assets.utils";
+import {ICalls} from "@/types/assets.type";
 
 interface IProps {
   handleClickGetIdDop?: (id?: number) => void;
@@ -17,12 +18,32 @@ interface IProps {
 const CallsTable2: FC<IProps> = ({ handleClickGetIdDop }) => {
   const dispatch = useAppDispatch();
   const dop = useTypedSelector((state) => state.pride.dop);
-
+  const [calls, setCalls] = useState<ICalls[][]>([]);
   useEffect(() => {
     if (dop) {
       dispatch(getDopThunk());
     }
   }, [dispatch]);
+
+
+  useEffect(() => {
+    if(dop) {
+      const groupByPlan = () => {
+        const groups: { [key: number]: ICalls[] } = {};
+        for (const item of dop) {
+          if (item.plan !== undefined) {
+            if (!groups[item.plan]) {
+              groups[item.plan] = [];
+            }
+            groups[item.plan].push(item);
+          }
+        }
+        return Object.values(groups);
+      };
+      setCalls(groupByPlan);
+    }
+  }, [dop]);
+
 
   const handleDeleteItems = async (id?: number) => {
     await instance
@@ -41,48 +62,72 @@ const CallsTable2: FC<IProps> = ({ handleClickGetIdDop }) => {
   };
 
   return (
-    <div className="main_table">
-      <div className="main_table-title">Доп.урок </div>
+      <div className="main_table">
+        <div className="main_table-title">Доп.урок</div>
 
-      <div className="main_table-block">
-        <Table>
-          <Thead>
-            <tr>
-              <Th>План звонков</Th>
-              <Th>Номер урока</Th>
-              <Th>Смена</Th>
-              <Th>Начало урока</Th>
-              <Th>Конец урока</Th>
-              <Th>Действие</Th>
-            </tr>
-          </Thead>
+        <div className="main_table-block" style={{display: "flex", flexWrap: "wrap", gap: "4%"}}>
+          {
+              calls &&
+              calls.map((call, index) => (
+                      <div style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "15px",
+                        width: "48%",
+                        padding: "21px",
+                        background: "#F4F4F4",
+                        borderRadius: "11.429px",
+                        marginBottom: "20px",
+                      }} key={index}>
+                        <div style={{color: "#D12E34", fontWeight: "bold"}}>
+                          План звонков: <span style={{color: "black"}}>{call[0].plan}</span>
+                        </div>
+                        <div style={{color: "#D12E34", fontWeight: "bold"}}>
+                          Смена: <span style={{color: "black"}}>{call[0].smena}</span>
+                        </div>
+                        <div>
 
-          {dop &&
-            dop.map((item, index) => (
-              <Tr key={item.plan}>
-                <Td>{item.plan}</Td>
-                <Td>{item.number}</Td>
-                <Td>{item.smena}</Td>
-                <Td>{removeSecondOfTime(item.start_time)}</Td>
-                <Td>{removeSecondOfTime(item.end_time)}</Td>
-                <Td>
-                  <div
-                    onClick={() =>
-                      handleClickGetIdDop && handleClickGetIdDop(item.id)
-                    }
-                  >
-                    <PenIcons />
-                  </div>
+                        </div>
+                        <Table>
+                          <Thead>
+                            <tr>
+                              <Th>№</Th>
+                              <Th>Начало урока</Th>
+                              <Th>Конец урока</Th>
+                              <Th>Действия</Th>
+                            </tr>
+                          </Thead>
 
-                  <div onClick={() => handleDeleteItems(item.id)}>
-                    <DeleteIcons />
-                  </div>
-                </Td>
-              </Tr>
-            ))}
-        </Table>
+                          {call &&
+                              call.map((item, index) => (
+                                  <Tr key={item.plan}>
+                                    <Td>{item.number}</Td>
+                                    <Td>{removeSecondOfTime(item.start_time)}</Td>
+                                    <Td>{removeSecondOfTime(item.end_time)}</Td>
+                                    <Td>
+                                      <div
+                                          onClick={() =>
+                                              handleClickGetIdDop && handleClickGetIdDop(item.id)
+                                          }
+                                      >
+                                        <PenIcons/>
+                                      </div>
+
+                                      <div onClick={() => handleDeleteItems(item.id)}>
+                                        <DeleteIcons/>
+                                      </div>
+                                    </Td>
+                                  </Tr>
+                              ))}
+                        </Table>
+                      </div>
+
+                  )
+              )
+          }
+
+        </div>
       </div>
-    </div>
   );
 };
 
